ea02a9a79a66eba37b629ac444d28a64c8843aeb
Ljupco1982 <54242904+Ljupco1982@users.noreply.github.com>
2020-04-04 17:53:54 +0200

    Updates

---
+++ precache-manifest.4802c9cabd05fe6ed2d68d5f55f7c221.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cfe5fd3ee95b0b1373a9838367f8774",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6ba23f99125a39f85998",
+    "url": "/monsters-rolodex/static/css/main.151bf699.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ba23f99125a39f85998",
+    "url": "/monsters-rolodex/static/js/main.c845a1ad.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
